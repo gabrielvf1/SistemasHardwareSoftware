3934479e84836810c2cfe3f8e80579243e7ca0ce
Gabriel Francato <vf_gabriel@yahoo.com.br>
2019-05-27 13:53:31 -0300

    posinx

--- lab-posix/src/example.c
+++ lab-posix/src/example.c
@@ -1,4 +1,5 @@
 #include "mintest/macros.h"
+#include <unistd.h>
 
 int test1() {
     test_printf("Hello! %d %f\n", 3, 3.14);
@@ -16,6 +17,63 @@ int test3() {
     return 0;
 }
 
-test_list = { TEST(test1), TEST(test2), TEST(test3) };
+int test4(){
+	int *ponteiro_null = NULL;
+	int a = *ponteiro_null*3;
+    return 0;
+}
+int test5(){
+	int a = 0;
+	int b =13/a;
+    return 0;
+}
+
+int test6(){
+	int a = 100000/4;
+	for (int i=0; i < a;i++){
+		for(int j=0; j<a;j++){
+			int b = 0;
+		}
+	}
+	printf("test6 Demora mas nao cai no Time Out \n");
+	return 1;
+}
+
+int test7(){
+	int a = 100000/4;
+	while(1){
+
+	}
+	return 0;
+}
+
+int test8(){
+	 test_assert(1 == 1, "Test Pass Assert 1\n");
+	 test_printf("[PASS] Passou Assert 1\n");
+	 test_assert(1 == 0, "Falhou Assert 2\n");
+	 //test_printf("test 8: Falhou Assert 2\n");
+	 test_assert(1 == 1, "Passou Assert 3\n");
+}
+
+int test9(){
+	int a = 2;
+	for (int i = 0; i < a ; i++){
+		test_printf("Print %d\n", i );
+	}
+	test_assert(1 == 1,"Passou");
+}
+int test10(){
+	 test_assert(1 == 1, "Test Rapido 1\n");
+	 test_printf("Rapido\n");
+	 sleep(1);
+	 test_assert(1 == 1, "Test Lento 1\n");	
+	 test_printf("Lento\n");
+	 //test_assert(1 == 1, "Test Rapido 1\n");
+
+}
+
+
+
+test_list = { TEST(test1), TEST(test2), TEST(test3), TEST(test4), TEST(test5), TEST(test6), TEST(test7), TEST(test8), TEST(test9), TEST(test10) };
 
 #include "mintest/runner.h"
--- lab-posix/src/mintest/runner.h
+++ lab-posix/src/mintest/runner.h
@@ -1,15 +1,45 @@
-
+    #include <sys/types.h>
+    #include <unistd.h>
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <sys/wait.h>
+    #include <string.h>
 
 int main() {
+    int status;
     int size = sizeof(all_tests)/sizeof(test_data);
     printf("Running %d tests:\n", size);
     printf("=====================\n\n");
     int pass_count = 0;
+    int pidF [size];
     for (int i = 0; i < size; i++) {
-        if (all_tests[i].function() >= 0) {
-            printf("%s: [PASS]\n", all_tests[i].name);
-            pass_count++;
-        };
+        if ((pidF[i] = fork()) == 0){
+            alarm(3);
+            if (all_tests[i].function() >= 0) {
+
+                exit(1);
+            }
+            else {
+                exit(0);
+            }
+
+        }
+    }
+    for (int i = 0; i < size; i++) {
+        pid_t atual = waitpid(pidF[i],&status,0);
+        if (WIFEXITED(status)){
+            if(WEXITSTATUS(status)){
+                printf("[PASS] %s Teste bem Sucedido\n",all_tests[i].name);
+                pass_count++;
+
+            }else{
+                printf("[FALHO] %s Teste Falhou \n",all_tests[i].name);
+            }
+        }
+        if(WIFSIGNALED(status) == 1){
+            printf("[FALHO] %s %s\n",all_tests[i].name,strsignal(WTERMSIG(status)));
+        }
+
     }
     
     printf("\n\n=====================\n");
